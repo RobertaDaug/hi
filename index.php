2bcdd4633f74d8f58668817ea9e823e168bf2f4f
RobertaDaug <118212798+RobertaDaug@users.noreply.github.com>
2022-11-17 18:47:13 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $vardas = "Roberta";
+    $pavarde = "Daug";
+    $gimimoData = 1984;
+    $sieMetai = 2022;
+
+    echo "Aš esu ". $vardas. " " . $pavarde. ". Man yra ". ($sieMetai - $gimimoData). " metai (ų).";
+
+    ?>
+
+    <p>Labas vakaras</p>
+</body>
+</html>
